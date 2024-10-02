99c495b14d3e2a90ada0bcbd01e5e3b907578170
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-03 07:10:26 +1000

    implemented counter for text area

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+import { MAXLENGTH } from "../lib/constants";
+
+
+
 export default function FeedbackForm() {
+  const [userInput, setUserInput] = useState("");
+
+  const charCount = MAXLENGTH - userInput.length;
+
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="null" spellCheck = {false} />
+      <textarea
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+        id="feedback-textarea"
+        placeholder="null"
+        spellCheck={false}
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
