e0b2d7a302232033421fd54fb235a404cdae7a41
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-03 08:37:16 +1000

    implemented feedbackItem

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,24 +1,34 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons"
+import FeedbackItem from "./FeedbackItem";
+
+const feedbackItem1 = {
+  upvoteCount: 593,
+  badgeLetter: "C",
+  companyName: "Company",
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam ipsa provident illum voluptatu",
+  daysAgo: 4
+}
+const feedbackItem2 = {
+  upvoteCount: 593,
+  badgeLetter: "S",
+  companyName: "Starbucks",
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam ipsa provident illum voluptatu",
+  daysAgo: 7
+}
+const feedbackItem3 = {
+  upvoteCount: 593,
+  badgeLetter: "C",
+  companyName: "Company",
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam ipsa provident illum voluptatu",
+  daysAgo: 10
+}
+
+const feedbackItems = [feedbackItem1, feedbackItem2, feedbackItem3];
+
 
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-            <TriangleUpIcon />
-            <span>593</span>
-        </button>
-        <div>
-            <p>C</p>
-
-        </div>
-        <div>
-            <p>Company</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam ipsa provident illum voluptatum omnis!</p>
-        </div>
-
-        <p>4</p>
-      </li>
+      {feedbackItems.map((feedbackItem) => <FeedbackItem feedbackItem={feedbackItem} />)}
     </ol>
   )
 }
