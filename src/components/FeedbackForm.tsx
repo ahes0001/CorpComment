46b3ad0567e9f3bd982dcffc0d726b2d64f3f045
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-03 07:13:24 +1000

    implemented validation for text area

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import { MAXLENGTH } from "../lib/constants";
 
-
-
 export default function FeedbackForm() {
   const [userInput, setUserInput] = useState("");
 
   const charCount = MAXLENGTH - userInput.length;
 
-
   return (
     <form className="form">
       <textarea
         value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
+        onChange={(e) => {
+          const inputValue = e.target.value;
+          if (inputValue.length <= MAXLENGTH) {
+            setUserInput(inputValue);
+          }
+        }}
         id="feedback-textarea"
         placeholder="null"
         spellCheck={false}
