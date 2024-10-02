9692f969d959eaf659e21a5fb3c8e51eab78e7e0
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-03 08:44:28 +1000

    implemented type for feedbackItem

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,6 +1,15 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 
-export default function FeedbackItem({feedbackItem}) {
+type FeedbackItem = {
+  upvoteCount: number;
+  badgeLetter: string;
+  companyName: string;
+  text: string;
+  daysAgo: string;
+};
+type FeedbackItemProps = {feedbackItem : FeedbackItem};
+
+export default function FeedbackItem({feedbackItem}: FeedbackItemProps) {
   return (
     <li className="feedback">
         <button>
