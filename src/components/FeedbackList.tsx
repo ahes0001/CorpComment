aed96654eb6c2babc8dcafa1740ff2be8cefaca4
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-03 14:05:25 +1000

    completed error catching for fetching data

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isloading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) =>  {
+        if(!response.ok) {
+          throw new Error("There was an error loading the feedbacks");
+        }
+        return response.json()})
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        // nerwork erro, nmot in 2xx response, or JOSN parsing error
+        setErrorMessage("There was an error loading the feedbacks");
         setIsLoading(false);
       });
   }, []);
@@ -22,6 +31,7 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isloading && <div className="spinner" />}
+      {errorMessage && <ErrorMessage message ={errorMessage} />}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key= {feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
