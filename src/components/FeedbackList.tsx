7e812092efa2badf2815097b3e5ef2cfe67853ab
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-03 10:53:03 +1000

    completed spinner for loading screen

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -3,8 +3,10 @@ import FeedbackItem from "./FeedbackItem";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
@@ -13,11 +15,13 @@ export default function FeedbackList() {
       })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <ol className="feedback-list">
+      {isloading && <div className="spinner" />}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key= {feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
