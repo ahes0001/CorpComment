31f0d9f1671f3c4c9fcebf0c36cf8a6973365e67
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-05 08:12:55 +1000

    swaped usage of the. to async await for learning

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -8,25 +8,50 @@ export default function FeedbackList() {
   const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-    )
-      .then((response) =>  {
-        if(!response.ok) {
-          throw new Error("There was an error loading the feedbacks");
-        }
-        return response.json()})
-      .then((data) => {
-        setFeedbackItems(data.feedbacks);
-        setIsLoading(false);
-      })
-      .catch(() => {
-        // nerwork erro, nmot in 2xx response, or JOSN parsing error
+    const fetchFeedbacksItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks");
+
+      if (!response.ok) {
+        throw new Error("There was an error loading the feedbacks");
+      }
+      const data = await response.json();
+      setFeedbackItems(data.feedbacks);
+        
+      } catch (error) {
         setErrorMessage("There was an error loading the feedbacks");
-        setIsLoading(false);
-      });
+      }
+      
+      setIsLoading(false);
+    };
+    
+
+    fetchFeedbacksItems()
   }, []);
+
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(
+  //     "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+  //   )
+  //     .then((response) =>  {
+  //       if(!response.ok) {
+  //         throw new Error("There was an error loading the feedbacks");
+  //       }
+  //       return response.json()})
+  //     .then((data) => {
+  //       setFeedbackItems(data.feedbacks);
+  //       setIsLoading(false);
+  //     })
+  //     .catch(() => {
+  //       // nerwork erro, nmot in 2xx response, or JOSN parsing error
+  //       setErrorMessage("There was an error loading the feedbacks");
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
   return (
     <ol className="feedback-list">
