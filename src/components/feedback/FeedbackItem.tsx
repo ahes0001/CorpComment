684e9a88e518f54fa5828cccbd93791ad1ea0a40
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-21 16:48:13 +1100

    complete upvote functionality

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -8,12 +8,19 @@ type FeedbackItemProps = {feedbackItem : TFeedbackItem};
 export default function FeedbackItem({feedbackItem}: FeedbackItemProps) {
 
   const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleUpVote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setUpvoteCount(upvoteCount+1)
+    e.currentTarget.disabled = true;
+    e.stopPropagation();
+  };
 
   return (
     <li onClick = {() => setOpen((prev)=> !prev)} className={`feedback ${open ? "feedback--expand": ""}`}>
-        <button>
+        <button onClick = {handleUpVote}>
             <TriangleUpIcon />
-            <span>{feedbackItem.upvoteCount}</span>
+            <span>{upvoteCount}</span>
         </button>
         <div>
             <p>{feedbackItem.badgeLetter}</p>
